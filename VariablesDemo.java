b706feaf87ebd00fe44ef7fee4d172a08da9ea5e
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2026-02-02 23:51:46 +0530

    [ADDED]ishwar: Variables in Java

---
+++ VariablesDemo.java
@@ -0,0 +1,22 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+
+        // Declaring variables
+        int age = 20;
+        double salary = 25000.50;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Ishwar";
+
+        // Printing variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+
+        // Changing value of variable
+        age = 21;
+        System.out.println("Updated Age: " + age);
+    }
+}
