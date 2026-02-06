496b5995659a037b997bb2a4bdd1458908abc4ff
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2026-02-06 23:40:39 +0530

    [ADDED]ishwar : If Else Statement in Java

---
+++ IfElse.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Age:");
+        int age = sc.nextInt();
+
+        if(age>=18){
+            System.out.println("Your Adult..!");
+        }
+        else{
+            System.out.println("Your Not Adult..!");
+        }
+
+    }
+}
