e2b334f7599220d7929b9fdf1ea06ff727deffaf
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2026-03-02 23:32:42 +0530

    [ADDED]ishwar: For Loop in Java

---
+++ ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop {
+    public static void main(String[] args) {
+
+        // Print numbers 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
