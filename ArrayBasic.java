b53e369b4f3ffec71c54ede365e2dcd1d4688251
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2026-03-08 23:29:53 +0530

    [ADDED]ishwar: Array in Java

---
+++ ArrayBasic.java
@@ -0,0 +1,19 @@
+public class ArrayBasic {
+    public static void main(String[] args) {
+
+        // Declare array
+        int[] numbers = new int[5];
+
+        // Assign values
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+        numbers[4] = 50;
+
+        // Access elements
+        System.out.println("First element: " + numbers[0]);
+        System.out.println("Second element: " + numbers[1]);
+        System.out.println("Third element: " + numbers[2]);
+    }
+}
