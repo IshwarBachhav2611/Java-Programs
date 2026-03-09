a21056a3f08afa7cbfce84fa339866fcdeb5b982
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2026-03-09 21:40:20 +0530

    [ADDED]ishwar: Array Initialization in Java

---
+++ ArrayInitialization.java
@@ -0,0 +1,9 @@
+class ArrayInitialization{
+    public static void main(String[] args){
+        int[] arr = {10,20,30,40,50};
+        System.out.println("Array Elements :");
+        for(int i = 0; i<5; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
