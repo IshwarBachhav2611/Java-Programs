6fa577f01baa54b112ff8dad0aa622b04b207471
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2026-03-10 22:01:24 +0530

    [ADDED]ishwar: ForEach on Array in java

---
+++ ArrayWithForEach.java
@@ -0,0 +1,8 @@
+public class ArrayWithForEach {
+    public static void main(String[] args){
+        int[] number = {10,20,30,40,50};
+        for (int num: number) {
+            System.out.println(num);
+        }
+    }
+}
