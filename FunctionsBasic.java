cae384ed4e7b279df956e7a95c8f23d33c49832f
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2026-03-14 23:05:59 +0530

    [ADDED]ishwar: Basic Function in JAva

---
+++ FunctionsBasic.java
@@ -0,0 +1,9 @@
+class FunctionsBasic{
+    public void message(){
+        System.out.println("Hello, World!");
+    }
+    public static void main(String[] args){
+        FunctionsBasic obj = new FunctionsBasic();
+        obj.message();
+    }
+}
