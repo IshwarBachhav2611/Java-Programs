37645c19390af0556060ab490ad4a0dbe3804826
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2026-03-26 23:46:12 +0530

    [ADDED]ishwar: Default Constructor in Java

---
+++ DefaultConstructor.java
@@ -0,0 +1,9 @@
+class DefaultConstructor {
+    DefaultConstructor(){
+        System.out.println("This is the Deafult Constructor in Java..!");
+    }    
+
+    public static void main(String[] args){
+        DefaultConstructor obj = new DefaultConstructor();
+    }
+}
