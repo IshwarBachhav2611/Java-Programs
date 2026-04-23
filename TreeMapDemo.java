c2fe6fa11751e09552be3be083ca707cb410a2fc
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2026-04-23 23:44:06 +0530

    [ADDED]ishwar: TreeMap in java Collections

---
+++ TreeMapDemo.java
@@ -0,0 +1,25 @@
+import java.util.TreeMap;
+
+public class TreeMapDemo {
+    public static void main(String[] args){
+        TreeMap<Integer,String> obj = new TreeMap<>();
+        obj.put(1, "Ishwar");
+        obj.put(2, "Shubahm");
+        obj.put(3,"Ritesh");
+        obj.put(4,"jayesh");
+        obj.put(5,"Umesh");
+
+        for(int i=1; i<=obj.size(); i++){
+            System.out.println("Key :"+i+" Value :"+obj.get(i));
+        }
+        
+        System.out.println("\nBefore Replace :"+obj.get(3));
+        obj.replace(3,"Dipak");
+        System.out.println("After Replace :"+obj.get(3));
+        
+        System.out.println("\nSize of HashMap :"+obj.size());
+        System.out.println("All Kays :"+obj.keySet());
+        System.out.println("All Values :"+obj.values());
+        
+    }
+}
